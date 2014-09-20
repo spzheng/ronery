ac07054f4ea2c2a0bd4cb0c978b7ecc7c7747dcd
hchen95 <hchen95@emory.edu>
2014-09-20 03:29:51 -0400

    Fix models primary keys

--- application/models.py
+++ application/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Person(models.Model):
-	phone = models.CharField() #instance of Field class, representing a db field
+	phone = models.CharField(unique = true, primary_key = true) 
 	active = models.BooleanField()
 	gender = models.CharField(max_length = 1)
 	age = models.IntegerField()
@@ -13,7 +13,7 @@ class Person(models.Model):
 		return self.title
 
 class Conversation (models.Model):
-	person1 = models.ForeignKey(Person)
+	person1 = models.ForeignKey(Person, primary_key = true)
 	person2 = models.ForeignKey(Person)
 	def __unicode__(self):
 		return self.post_text
